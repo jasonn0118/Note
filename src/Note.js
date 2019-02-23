d5a93953d95590f787c4e0a199ca6fd0e83f3e4f
jasonn0118 <tlswoals2006@gmail.com>
2019-02-23 11:23:06 -0800

    first commit

---
+++ src/Note.js
@@ -0,0 +1,68 @@
+import React, {Component} from 'react';
+import {FaPencilAlt} from 'react-icons/fa';
+import {FaTrash} from 'react-icons/fa';
+
+
+class Note extends Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            editing: false
+        }
+        this.edit=this.edit.bind(this)
+        this.remove=this.remove.bind(this)
+        this.save = this.save.bind(this)
+        this.renderForm=this.renderForm.bind(this)
+        this.renderDisplay=this.renderDisplay.bind(this)
+        
+    }
+
+    edit(){
+        this.setState({
+            editing:true
+        })
+    }
+    remove(){
+        this.props.onRemove(this.props.index)
+    }
+    save(e){
+        e.preventDefault()
+        console.log(e.target.value);
+        this.props.onChange(this._newText.value,this.props.index)
+        this.setState({
+            editing:false
+        })
+    }
+    change123= e => {
+        // console.log('fjewoifjewofi');
+        console.log(e.target.value);
+    }
+    renderForm(){
+        return(
+            <div className="note">
+                <form onSubmit={this.save}>
+                    <textarea ref={input=>this._newText=input}/>
+                    <input onChange={this.change123}/>
+                    <button id="save">FLOPPY-O</button>
+                </form>
+            </div>
+        )
+    }
+
+    renderDisplay(){
+        return(
+            <div className="note">
+                <p>{this.props.children}</p>
+                <span>
+                    <button onClick={this.edit} id ="edit"><FaPencilAlt/></button>
+                    <button onClick={this.remove} id = "remove"><FaTrash/></button>
+                </span>
+            </div>
+        )
+    }
+    render(){
+        return this.state.editing? this.renderForm():this.renderDisplay();
+    }
+}
+
+export default Note;
